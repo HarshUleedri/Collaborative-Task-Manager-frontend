f86b22eb254f7f944111aae0124aaae35a39bbda
HarshUleedri <ulidari2@gmail.com>
2025-07-05 12:36:30 +0530

    feat: :sparkles: login credential added in login page for testing purpose

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -30,8 +30,43 @@ const PatientLogin = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="h-screen flex items-center justify-center px-4 "
+      className="h-screen flex flex-col items-center justify-center px-4 "
     >
+      {/* for testing  */}
+      <div className="border border-destructive rounded-md  p-2 mb-6 w-full lg:w-1/3">
+        <h3 className="text-xl text-destructive font-semibold mb-2">
+          Credentials for Testing Purpose{" "}
+        </h3>
+        <div className="space-y-2">
+          <div>
+            <p className="font-semibold text-sm">
+              Email For <span className="font-bold">Member</span> :
+              huleedri@gmail.com
+            </p>
+            <p className="font-semibold text-sm">
+              Password For <span className="font-bold">Member</span> : 123456
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-sm">
+              Email For <span className="font-bold">Manager</span> :
+              uleedri@gmail.com
+            </p>
+            <p className="font-semibold text-sm">
+              Password For <span className="font-bold">Manager</span> : 123456
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-sm">
+              Email For <span className="font-bold">Admin</span> :
+              ruleedri@gmail.com
+            </p>
+            <p className="font-semibold text-sm">
+              Password For <span className="font-bold">Admin</span> : 123456
+            </p>
+          </div>
+        </div>
+      </div>
       <Card className="w-full lg:w-1/3  ">
         <CardHeader className="mb-1">
           <CardTitle>
